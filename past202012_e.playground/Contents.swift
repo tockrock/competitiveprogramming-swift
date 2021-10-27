88325d85bbde6e478b3b03d256e636c0f9d36c13
Takeshi Suzuki <tockrock@gmail.com>
2021-10-27 15:40:07 +0900

    going to forget about this now and start with the actual starting problems

--- past202012_e.playground/Contents.swift
+++ past202012_e.playground/Contents.swift
@@ -2,4 +2,43 @@
 
 import Foundation
 
+let sampleInput = """
+3 3
+...
+.#.
+..#
+#.#
+###
+...
+"""
+var workingInput = sampleInput.split(separator: "\n")
+var standardIO: Bool?
+
+func myReadLine() -> String? {
+    if standardIO == nil or standardIO! = true {
+        let input = readLine()
+        if input != nil {
+            standardIO = true
+            return input
+        }
+    }
+    let input = readLine()
+    if input != nil {
+        
+    }
+}
+
+extension workingInput {
+    func popFirst() -> Element {
+        
+    }
+}
+
+let definitionRaw = readLine()
+let definition = definitionRaw != nil ? definitionRaw! : String(workingInput.first!)
+workingInput.dropFirst()
+
+let definitionArray = definition.split(separator: " ")
+let h = definitionArray[0]
+let w = definitionArray[1]
 
