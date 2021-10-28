17ea91ee605c51d71118994666dff035ff07bdf8
Takeshi Suzuki <tockrock@gmail.com>
2021-10-29 08:47:32 +0900

    Basic implementation

--- practice_2.playground/Contents.swift
+++ practice_2.playground/Contents.swift
@@ -1,3 +1,30 @@
-import Cocoa
+// https://atcoder.jp/contests/practice/tasks/practice_2
+// 2021-10-29 07:55:19
 
-var greeting = "Hello, playground"
+import Foundation
+
+func readSubsequence () -> [String.SubSequence] { readLine()!.split(separator: " ")}
+func readInts() -> [Int] { readSubsequence().map{Int($0)!} }
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readInts()
+    return (a: ints[0], b: ints[1])
+}
+
+let (N, Q) = readTwoInts()
+
+var list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".map({$0})[0..<5].map({String($0)})
+list.sort(by: {
+    print("? \($0) \($1)")
+    let result = readLine()!
+    switch(result) {
+    case ">" :
+        return $0 > $1
+    case "<" :
+        return $0 > $1
+    default:
+        return true
+    }
+})
+
+print("!", list.joined())
