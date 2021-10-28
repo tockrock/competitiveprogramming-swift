1f84b5651738dfe27eeeaf589caad9294bb0eb3d
Takeshi Suzuki <tockrock@gmail.com>
2021-10-28 13:02:52 +0900

    solution for abc087_b

--- abc087_b.playground/Contents.swift
+++ abc087_b.playground/Contents.swift
@@ -11,13 +11,43 @@ struct Example {
 let examples: [(String, Example)] = [
     ("1", Example(
         input: """
+            2
+            2
+            2
+            100
             """,
         expected: """
+            2
+            """)),
+    ("original", Example(
+        input: """
+            3
+            10
+            6
+            1350
+            """,
+        expected: """
+            6
             """)),
     ("2", Example(
         input: """
+            5
+            1
+            0
+            150
             """,
         expected: """
+            0
+            """)),
+    ("3", Example(
+        input: """
+            30
+            40
+            50
+            6000
+            """,
+        expected: """
+            213
             """)),
 ]
 
@@ -25,6 +55,26 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // actual code goes here
     func readInts() -> [Int] { readLine()!.split(separator: " ").map{Int($0)!} }
     func readInt() -> Int { Int(readLine()!)! }
+    
+    let givenFiveHundreds = readInt()
+    let givenHundreds = readInt()
+    let givenFifties = readInt()
+    let target = readInt()
+    
+    var count = 0
+    
+    for i in 0...givenFiveHundreds {
+        for j in 0...givenHundreds {
+            for k in 0...givenFifties {
+                if target == i * 500 + j * 100 + k * 50 {
+                    count += 1
+                }
+            }
+        }
+    }
+    
+    print(count)
+    
     
     
 }
