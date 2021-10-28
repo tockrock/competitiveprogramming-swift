4c830b869a6f5612eb50e56b59590d9e77db1834
Takeshi Suzuki <tockrock@gmail.com>
2021-10-28 13:03:59 +0900

    template

--- abc083_b.playground/Contents.swift
+++ abc083_b.playground/Contents.swift
@@ -1,3 +1,61 @@
-import Cocoa
+// https://atcoder.jp/contests/abs/tasks/abc083_b
+// 2021-10-28
 
-var greeting = "Hello, playground"
+import Foundation
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            """,
+        expected: """
+            """)),
+    ("2", Example(
+        input: """
+            """,
+        expected: """
+            """)),
+]
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    // actual code goes here
+    func readInts() -> [Int] { readLine()!.split(separator: " ").map{Int($0)!} }
+    func readInt() -> Int { Int(readLine()!)! }
+    
+    
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines .isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
+
