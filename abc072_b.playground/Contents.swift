7e6eba24535184a57c79a83ed73c4a458294f455
Takeshi Suzuki <tockrock@gmail.com>
2021-11-04 08:28:10 +0900

    Start abc072_b

---
+++ abc072_b.playground/Contents.swift
@@ -0,0 +1,83 @@
+// https://atcoder.jp/contests/abc072/tasks/abc072_b
+// 2021-11-04 08:27:56
+
+import Foundation
+let DEBUG = true
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            """,
+        expected: """
+            """)),
+    ("2", Example(
+        input: """
+            """,
+        expected: """
+            """)),
+    ("3", Example(
+        input: """
+            """,
+        expected: """
+            """)),
+]
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    // =====================
+    // actual code goes here
+    // =====================
+    func debugPrint (_ s: String ) { if DEBUG { Swift.print(s) } }
+    func readSubsequence () -> [String.SubSequence] { readLine()!.split(separator: " ")}
+    func readStrings () -> [String] { readSubsequence().map({String($0)}) }
+    func readInts() -> [Int] { readSubsequence().map{Int($0)!} }
+    func readInt() -> Int { Int(readLine()!)! }
+    func readTwoInts() -> (a: Int, b: Int) {
+        let ints = readInts()
+        return (a: ints[0], b: ints[1])
+    }
+    func readThreeInts() -> (a: Int, b: Int, c: Int) {
+        let ints = readInts()
+        return (a: ints[0], b: ints[1], c: ints[2])
+    }
+    
+    print("foo")
+    
+    // ===============
+    // actual code end
+    // ===============
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines.isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
