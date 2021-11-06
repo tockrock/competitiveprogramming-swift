b7ca382ff7008da85f0435a497476b9a13fa8a07
Takeshi Suzuki <tockrock@gmail.com>
2021-11-07 07:58:32 +0900

    Tried implementing factor but using a different approach

--- abc057_c.playground/Contents.swift
+++ abc057_c.playground/Contents.swift
@@ -52,8 +52,47 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // actual code goes here
     // =====================
     
-    print("foo")
     
+    var N = readInt()
+    
+    // get an Array of Factors
+
+    func findFactor(_ base: Int, divisibleBy factor: Int) -> (remaining: Int, factors: [Int]) {
+        var remaining = base
+        var count = 0
+        while true {
+            if remaining % factor == 0 {
+                remaining /= factor
+                count += 1
+            } else {
+                break
+            }
+        }
+        
+        return (remaining, Array(repeatElement(factor, count: count)))
+    }
+
+    var factors = [[Int]]()
+    let twos: [Int]
+    (N, twos) = findFactor(N, divisibleBy: 2)
+    
+    factors.append(twos)
+    
+    for i in stride(from: 3, to: Int(sqrt(Double(N))), by: 2) {
+        let factorArray: [Int]
+        (N, factorArray) = findFactor(N, divisibleBy: i)
+        if !factorArray.isEmpty { factors.append(factorArray) }
+        if i > Int(sqrt(Double(N))) {
+            break
+        }
+    }
+    
+    // try all combinations of factors and get the minimum result
+    
+    
+    
+    print(factors)
+
     // ===============
     // actual code end
     // ===============
