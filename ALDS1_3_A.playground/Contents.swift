76270660d9ffeea7fe51fe91d8abc091dc6b97ac
Takeshi Suzuki <tockrock@gmail.com>
2021-11-10 15:12:08 +0900

    Basic ALDS1_3_A

---
+++ ALDS1_3_A.playground/Contents.swift
@@ -0,0 +1,106 @@
+// Book: ALDS1_3_A: Stack
+// 2021-11-10 14:58:35
+
+import Foundation
+let DEBUG = true
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            1 2 + 3 4 - *
+            """,
+        expected: """
+            -3
+            """)),
+]
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    func myDebugPrint (_ s: Any... ) { if DEBUG { Swift.print(s.map({"\($0)"}).joined(separator: " ")) } }
+    func readString () -> String { readLine()! }
+    func readSubsequence () -> [String.SubSequence] { readString().split(separator: " ")}
+    func readCharStrings () -> [String] {readString().map({String($0)})}
+    func readStrings () -> [String] { readSubsequence().map({String($0)}) }
+    func readInt() -> Int { Int(readString())! }
+    func readInts() -> [Int] { readSubsequence().map{Int($0)!} }
+    func readTwoInts() -> (Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1])
+    }
+    func readThreeInts() -> (Int, Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1], ints[2])
+    }
+    // =====================
+    // actual code goes here
+    // =====================
+    
+    let input = readStrings()
+    
+    var stack = [String]()
+    
+    for i in input {
+        if Int(i) != nil {
+            stack.append(i)
+            continue
+        }
+        let first = Int(stack.popLast()!)!
+        let second = Int(stack.popLast()!)!
+        let result: Int
+        
+        switch(i) {
+        case "+":
+            result = second + first
+        case "-":
+            result = second - first
+        case "*":
+            result = second * first
+        case "/":
+            result = second / first
+        default:
+            result = 3
+        }
+        stack.append(String(result))
+    }
+    
+    print(stack[0])
+    
+    // ===============
+    // actual code end
+    // ===============
+    
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines.isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
