3be6a7aded2c1779b5f77620c2031afb9364cc67
Takeshi Suzuki <tockrock@gmail.com>
2021-11-17 08:44:31 +0900

    Start problem from other sites

---
+++ other2021-11-17.playground.playground/Contents.swift
@@ -0,0 +1,93 @@
+// https://qiita.com/drken/items/0c88a37eec520f82b788
+// problem 4
+// 2021-11-17 08:37:07
+
+import Foundation
+let DEBUG = true
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            1 1 5 5
+            """,
+        expected: """
+            3
+            """)),
+]
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    // this needs to be in run() to get the overwritten readLine()
+    func myDebugPrint (_ s: Any... ) { if DEBUG { Swift.print(s.map({"\($0)"}).joined(separator: " ")) } }
+    func readString () -> String { readLine()! }
+    func readSubsequence () -> [String.SubSequence] { readString().split(separator: " ")}
+    func readChars () -> [Character] {readString().map({$0})}
+    func readStrings () -> [String] { readSubsequence().map({String($0)}) }
+    func readInt() -> Int { Int(readString())! }
+    func readInts() -> [Int] { readSubsequence().map{Int($0)!} }
+    func readTwoInts() -> (Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1])
+    }
+    func readThreeInts() -> (Int, Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1], ints[2])
+    }
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        if b == 0 { return a }
+        return gcd(b, a%b)
+    }
+    
+    // =====================
+    // actual code goes here
+    // =====================
+    
+    let input = readInts()
+    let x1 = input[0], y1 = input[1], x2 = input[2], y2 = input[3]
+    
+    let x = abs(x2 - x1)
+    let y = abs(y2 - y1)
+    
+    let common = gcd(x, y)
+    print(common - 1)
+    
+    // ===============
+    // actual code end
+    // ===============
+    
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        // print(outputLines, expectedLines)
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines.isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
