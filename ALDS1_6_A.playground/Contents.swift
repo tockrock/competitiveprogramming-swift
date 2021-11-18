d50cceb8dbd766d992474fd5babb1a5ca20ee271
Takeshi Suzuki <tockrock@gmail.com>
2021-11-19 08:40:04 +0900

    Sample

--- ALDS1_6_A.playground/Contents.swift
+++ ALDS1_6_A.playground/Contents.swift
@@ -12,8 +12,11 @@ struct Example {
 let examples: [(String, Example)] = [
     ("1", Example(
         input: """
+            7
+            2 5 1 3 2 3 0
             """,
         expected: """
+            0 1 2 2 3 3 5
             """)),
 ]
 
