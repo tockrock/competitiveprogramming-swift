83bef6bbd925298bcbc83696aaf216b8bd0ea701
Takeshi Suzuki <tockrock@gmail.com>
2021-11-25 10:13:15 +0900

    More sample

--- ALDS1_7_D.playground/Contents.swift
+++ ALDS1_7_D.playground/Contents.swift
@@ -19,6 +19,15 @@ let examples: [(String, Example)] = [
         expected: """
             3 4 2 5 1
             """)),
+    ("2", Example(
+        input: """
+            9
+            1 2 3 4 5 6 7 8 9
+            3 2 5 4 6 1 8 7 9
+            """,
+        expected: """
+            3 5 6 4 2 8 9 7 1
+            """)),
 ]
 
 func run(readLine: () -> String?, print: (Any...) -> Void) {
