5e4a853a62fcf4036eaefa6d4d76fa977274274b
Takeshi Suzuki <tockrock@gmail.com>
2021-12-08 08:37:59 +0900

    Start ALDS1_10_B: Matrix Chain Multiplication

---
+++ ALDS1_10_B.playground/Contents.swift
@@ -0,0 +1,129 @@
+// Book: ALDS1_10_B: Matrix Chain Multiplication
+// 2021-12-07 10:22:28
+
+import Foundation
+let DEBUG = true
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            6
+            30 35
+            35 15
+            15 5
+            5 10
+            10 20
+            20 25
+            """,
+        expected: """
+            15125
+            """)),
+]
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    // these needs to be in run() to get the overwritten readLine()
+    func readString () -> String { readLine()! }
+    func readSubsequence () -> [String.SubSequence] { readString().split(separator: " ")}
+    func readChars () -> [Character] {readString().map({$0})}
+    func readStrings () -> [String] { readSubsequence().map({String($0)}) }
+    func readInt() -> Int { Int(readString())! }
+    func readInts() -> [Int] { readSubsequence().map{Int($0)!} }
+    func readTwoInts() -> (Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1])
+    }
+    func readThreeInts() -> (Int, Int, Int) {
+        let ints = readInts()
+        return (ints[0], ints[1], ints[2])
+    }
+    
+    // =====================
+    // actual code goes here
+    // =====================
+    let N = 100
+    let n = readInt()
+    
+    var m = [[Int]](repeating: [Int](repeating: 0, count: N+1), count: N+1)
+    
+    var p = readInts()
+    for _ in 2...n {
+        let (_, next) = readTwoInts()
+        p.append(next)
+    }
+    
+    for l in 2...n {
+        for i in 1...(n-l+1) {
+            let j = i  + l - 1
+            m[i][j] = (1 << 21)
+            
+        }
+    }
+    
+    // ===============
+    // actual code end
+    // ===============
+    
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        // print(outputLines, expectedLines)
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines.isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
+func myDebugPrint (_ s: Any... ) { if DEBUG { Swift.print(s.map({"\($0)"}).joined(separator: " ")) } }
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 { return a }
+    return gcd(b, a%b)
+}
+func pf(_ numb: Int) -> [(prime: Int, count: Int)] {
+    var n = numb, i = 2, primeList = [(Int, Int)]()
+    while i * i <= numb && i <= n {
+        var count = 0
+        while n % i == 0 { n /= i; count += 1 }
+        if count > 0 { primeList.append((i, count)) }
+        i += 1
+    }
+    if n != 1 { primeList.append((n, 1)) }
+    return primeList
+}
+
+// Only for the Book
+extension Array {
+    func outputWithSpace() -> String {
+        return String(
+            self.reduce("") {
+                $0 + "\($1) "
+            }.dropLast(1)
+        )
+    }
+}
