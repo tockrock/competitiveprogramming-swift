36265548a17bfd3c2826e0de2d16f6d26ec16745
Takeshi Suzuki <tockrock@gmail.com>
2021-12-25 21:00:53 +0900

    Start abc233_a

--- abc233_a.playground/Contents.swift
+++ abc233_a.playground/Contents.swift
@@ -1,5 +1,5 @@
-// https://atcoder.jp/contests/abc232/tasks/abc232_c
-// 2021-12-19 21:53:13
+// https://atcoder.jp/contests/abc233/tasks/abc233_a
+// 2021-12-25 21:00:35
 
 import Foundation
 let DEBUG = true
@@ -12,44 +12,18 @@ struct Example {
 let examples: [(String, Example)] = [
     ("1", Example(
         input: """
-            4 4
-            1 2
-            1 3
-            1 4
-            3 4
-            1 3
-            1 4
-            2 3
-            3 4
             """,
         expected: """
-            Yes
             """)),
     ("2", Example(
         input: """
-            5 6
-            1 2
-            1 3
-            1 4
-            3 4
-            3 5
-            4 5
-            1 2
-            1 3
-            1 4
-            1 5
-            3 5
-            4 5
             """,
         expected: """
-            No
             """)),
     ("3", Example(
         input: """
-            8 0
             """,
         expected: """
-            Yes
             """)),
 ]
 
@@ -74,40 +48,7 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // actual code goes here
     // =====================
     
-    // reference: https://atcoder.jp/contests/abc232/editorial/3143
-    let (n, m) = readTwoInts()
-    var toy1 = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
-    for _ in 0..<m {
-        let(a, b) = readTwoInts()
-        toy1[a-1][b-1] = true
-        toy1[b-1][a-1] = true
-    }
-    myDebugPrint(toy1)
-    
-    var toy2 = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
-    for _ in 0..<m {
-        let (c, d) = readTwoInts()
-        toy2[c-1][d-1] = true
-        toy2[d-1][c-1] = true
-    }
-    myDebugPrint(toy2)
-    
-    var ans = false
-
-    overall: for list in permutation(Array(0..<n)) {
-        for i in 0..<n {
-            for j in 0..<n{
-                if toy1[i][j] != toy2[list[i]][list[j]] {
-                    continue overall
-                }
-            }
-        }
-        ans = true
-        break
-    }
-    
-    print(ans ? "Yes" : "No")
-    
+    print("foo")
     
     // ===============
     // actual code end
