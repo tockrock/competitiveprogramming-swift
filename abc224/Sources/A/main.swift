10e8ccb0185320bb2931c8fe4fbe043bda02f203
Takeshi Suzuki <tockrock@gmail.com>
2022-02-12 20:53:15 +0900

    Finish abc224a

--- abc224/Sources/A/main.swift
+++ abc224/Sources/A/main.swift
@@ -2,3 +2,11 @@
 // https://atcoder.jp/contests/abc224/tasks/abc224_a
 // 実行制限時間: 2.0 sec
 import Foundation
+
+let S = readLine()!.map { $0 }
+
+if S[S.count-2] == "e" {
+    print("er")
+} else {
+    print("ist")
+}
