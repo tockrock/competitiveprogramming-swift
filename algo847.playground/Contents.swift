3c392c4f667945f6cb6555f202b532af59a1e0f2
Takeshi Suzuki <tockrock@gmail.com>
2022-03-31 13:34:34 +0900

    algo847 - Move the DeQue struct
    This now works somehow...

--- algo847.playground/Contents.swift
+++ algo847.playground/Contents.swift
@@ -34,6 +34,70 @@ let examples: [(String, Example)] = [
 // Remember to paste these as well!!
 // =================================
 
+struct DeQue<T> {
+    private class Node<T> {
+        let value: T
+        var previous: Node?
+        var next: Node?
+        
+        init(value: T, previous: Node? = nil, next: Node? = nil) {
+            self.value = value
+            self.previous = previous
+            self.next = next
+        }
+    }
+    
+    private var head: Node<T>? = nil
+    private var tail: Node<T>? = nil
+    
+    mutating func pushHead(_ value: T) {
+        head = Node(value: value, next: head)
+        
+        if let next = head?.next {
+            next.previous = head
+        } else {
+            tail = head
+        }
+    }
+    
+    mutating func pushTail(_ value: T) {
+        tail = Node(value: value, previous: tail)
+        
+        if let previous = tail?.previous {
+            previous.next = tail
+        } else {
+            head = tail
+        }
+    }
+    
+    mutating func popHead() -> T? {
+        guard let oldHead = head else { return nil }
+        
+        if let newHead = oldHead.next {
+            newHead.previous = nil
+            head = newHead
+        } else {
+            head = nil
+            tail = nil
+        }
+        
+        return oldHead.value
+    }
+    
+    mutating func popTail() -> T? {
+        guard let oldTail = tail else { return nil }
+        
+        if let newTail = oldTail.previous {
+            newTail.next = nil
+            tail = newTail
+        } else {
+            head = nil
+            tail = nil
+        }
+        return oldTail.value
+    }
+}
+
 func run(readLine: () -> String?, print: (Any...) -> Void) {
     // =====================
     // actual code goes here
@@ -46,70 +110,6 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
         case pushTail = "1"
         case popHead = "2"
         case popTail = "3"
-    }
-    
-    struct DeQue<T> {
-        private class Node<T> {
-            let value: T
-            var previous: Node?
-            var next: Node?
-
-            init(value: T, previous: Node? = nil, next: Node? = nil) {
-                self.value = value
-                self.previous = previous
-                self.next = next
-            }
-        }
-
-        private var head: Node<T>? = nil
-        private var tail: Node<T>? = nil
-
-        mutating func pushHead(_ value: T) {
-            head = Node(value: value, next: head)
-
-            if let next = head?.next {
-                next.previous = head
-            } else {
-                tail = head
-            }
-        }
-
-        mutating func pushTail(_ value: T) {
-            tail = Node(value: value, previous: tail)
-
-            if let previous = tail?.previous {
-                previous.next = tail
-            } else {
-                head = tail
-            }
-        }
-
-        mutating func popHead() -> T? {
-            guard let oldHead = head else { return nil }
-
-            if let newHead = oldHead.next {
-                newHead.previous = nil
-                head = newHead
-            } else {
-                head = nil
-                tail = nil
-            }
-
-            return oldHead.value
-        }
-
-        mutating func popTail() -> T? {
-            guard let oldTail = tail else { return nil }
-
-            if let newTail = oldTail.previous {
-                newTail.next = nil
-                tail = newTail
-            } else {
-                head = nil
-                tail = nil
-            }
-            return oldTail.value
-        }
     }
 
     var deque = DeQue<String>()
