fed3e6a1c6a51eb48a4bc1dded5e0c2611b2d31f
Takeshi Suzuki <tockrock@gmail.com>
2022-04-14 09:29:18 +0900

    algo865 - Start

---
+++ algo865.playground/Contents.swift
@@ -0,0 +1,98 @@
+// https://algo-method.com/tasks/865
+// 2022-04-14 09:28:19
+
+import Foundation
+
+struct Example {
+    let input: String
+    let expected: String
+}
+
+let examples: [(String, Example)] = [
+    ("1", Example(
+        input: """
+            6
+            3 1 4 1 5 9
+            3
+            3
+            1
+            7
+            """,
+        expected: """
+            2
+            6
+            0
+            """)),
+    ("2", Example(
+        input: """
+            5
+            51 13 26 91 102
+            5
+            13
+            2
+            17
+            2
+            3
+            """,
+        expected: """
+            3
+            2
+            2
+            2
+            2
+            """)),
+]
+
+// =================================
+// Extensions
+// Remember to paste these as well!!
+// =================================
+
+func run(readLine: () -> String?, print: (Any...) -> Void) {
+    // =====================
+    // actual code goes here
+    // =====================
+    
+    // let mod = 1000000007
+    // let mod = 998244353
+    
+    print("foo")
+    
+    // ===============
+    // actual code end
+    // ===============
+}
+
+func main(label: String, example: Example) {
+    var inputLines = example.input.split(separator: "\n")
+    var outputLines: [String] = []
+    defer {
+        var expectedLines = example.expected
+            .split(separator: "\n")
+            .map(String.init)
+        let isSuccessful = expectedLines == outputLines
+        print("== Test[\(label)] =========")
+        print(isSuccessful ? "successful." : "failed.")
+        // print(outputLines, expectedLines)
+        if !isSuccessful {
+            print("expected | actual")
+            while !outputLines.isEmpty && !expectedLines.isEmpty {
+                let o = outputLines.removeFirst()
+                let e = expectedLines.removeFirst()
+                print("\(o == e ? " " : "!") \(e) | \(o)")
+            }
+        }
+        print("")
+    }
+    run(
+        readLine: { String(inputLines.removeFirst()) },
+        print: { outputLines.append($0.map {"\($0)"}.joined(separator: " ")) }
+    )
+}
+
+for example in examples {
+    main(label: example.0, example: example.1)
+}
+
+func myDebugPrint (_ s: Any... ) { Swift.print(s.map {"\($0)"}.joined(separator: " ")) }
+
