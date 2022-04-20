5733c5a2f83c53da4935cff620c1518c65cd15ac
Takeshi Suzuki <tockrock@gmail.com>
2022-04-20 09:19:46 +0900

    algo871 - AC

--- algo871.playground/Contents.swift
+++ algo871.playground/Contents.swift
@@ -50,10 +50,40 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // actual code goes here
     // =====================
     
-    // let mod = 1000000007
-    // let mod = 998244353
+    func readInts() -> [Int] { readLine()!.split(separator: " ").map { Int(String($0))! } }
+
+    enum Command: Int {
+        case follow, unfollow, countSameFollowers
+    }
     
-    print("foo")
+    let input = readInts()
+    let N = input[0], Q = input[1]
+    
+    var followersOf = [Set<Int>](repeating: [], count: N)
+    var followerSetCount = [Set<Int>: Int]()
+    followerSetCount[[]] = N
+    
+    for _ in 0..<Q {
+        let query = readInts()
+        let rawCommandValue = query[0]
+        switch Command(rawValue: rawCommandValue)! {
+        case .follow:
+            let x = query[1], y = query[2]
+            guard !followersOf[y].contains(x) else { continue }
+            followerSetCount[followersOf[y]]! -= 1
+            followersOf[y].insert(x)
+            followerSetCount[followersOf[y], default: 0] += 1
+        case .unfollow:
+            let x = query[1], y = query[2]
+            guard followersOf[y].contains(x) else { continue }
+            followerSetCount[followersOf[y]]! -= 1
+            followersOf[y].remove(x)
+            followerSetCount[followersOf[y], default: 0] += 1
+        case .countSameFollowers:
+            let z = query[1]
+            print(followerSetCount[followersOf[z]]! - 1)
+        }
+    }
     
     // ===============
     // actual code end
