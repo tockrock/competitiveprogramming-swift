5a8442f1e1e232e13c8abc0f7249118b1b2f7787
Takeshi Suzuki <tockrock@gmail.com>
2022-05-09 20:38:00 +0900

    algo905 - AC

--- algo905.playground/Contents.swift
+++ algo905.playground/Contents.swift
@@ -11,13 +11,19 @@ struct Example {
 let examples: [(String, Example)] = [
     ("1", Example(
         input: """
+            7
+            0 0 1 1 4 2
             """,
         expected: """
+            0 1 3 4 5 2 6
             """)),
     ("2", Example(
         input: """
+            10
+            0 1 2 3 4 1 2 3 4
             """,
         expected: """
+            0 1 2 3 4 5 9 8 7 6
             """)),
 ]
 
@@ -31,10 +37,27 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // actual code goes here
     // =====================
     
-    // let mod = 1000000007
-    // let mod = 998244353
+    func readInts() -> [Int] { readLine()!.split(separator: " ").map { Int(String($0))! } }
     
-    print("foo")
+    let N = Int(readLine()!)!
+    let As = readInts()
+    
+    var tree = [[Int]](repeating: [], count: N)
+    
+    for i in 0..<N-1 {
+        let A = As[i]
+        tree[A].append(i+1)
+    }
+        
+    var ans = [String]()
+    func preorder(i: Int) {
+        ans.append(String(i))
+        for child in tree[i] {
+            preorder(i: child)
+        }
+    }
+    preorder(i: 0)
+    print(ans.joined(separator: " "))
     
     // ===============
     // actual code end
