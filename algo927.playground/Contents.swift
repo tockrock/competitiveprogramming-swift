c613c168091531fbdb90c0543fbfe3384c620af5
Takeshi Suzuki <tockrock@gmail.com>
2022-05-25 08:14:33 +0900

    algo927 - AC

--- algo927.playground/Contents.swift
+++ algo927.playground/Contents.swift
@@ -42,11 +42,23 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
     // =====================
     // actual code goes here
     // =====================
+        
+    var heap = Heap<Task>()
+    let Q = Int(readLine()!)!
     
-    // let mod = 1000000007
-    // let mod = 998244353
-    
-    print("foo")
+    for i in 0..<Q {
+        let queue = readLine()!.split(separator: " ")
+        let rawCommand = Int(String(queue[0]))!
+        switch Command(rawValue: rawCommand)! {
+        case .insert:
+            let deadline = Int(String(queue[1]))!
+            let taskName = String(queue[2])
+            let task = Task(deadline: deadline, queue: i, task: taskName)
+            heap.insert(element: task)
+        case .pop:
+            print(heap.pop()?.task ?? "-1")
+        }
+    }
     
     // ===============
     // actual code end
@@ -58,6 +70,63 @@ func run(readLine: () -> String?, print: (Any...) -> Void) {
 // Remember to paste these as well!!
 // =================================
 
+struct Task: Comparable {
+    let deadline: Int
+    let queue: Int
+    let task: String
+    
+    static func < (lhs: Task, rhs: Task) -> Bool {
+        guard lhs.deadline != rhs.deadline else {
+            return lhs.queue < rhs.queue
+        }
+        return lhs.deadline < rhs.deadline
+    }
+}
+
+enum Command: Int {
+    case insert, pop
+}
+
+struct Heap<T: Comparable> {
+    private(set) var data = [T]()
+
+    mutating func insert(element: T) {
+        data.append(element)
+        swim(index: data.endIndex - 1)
+    }
+    
+    mutating func pop() -> T? {
+        guard !data.isEmpty else { return nil }
+        data.swapAt(0, data.endIndex - 1)
+        defer { sink(index: 0) }
+        return data.popLast()
+    }
+    
+    private func order(lhs: T, rhs: T) -> Bool {
+        return lhs < rhs
+    }
+    
+    mutating private func swim(index: Int) {
+        guard index > 0 else { return }
+        let parentIndex = (index - 1) / 2
+        guard !order(lhs: data[parentIndex], rhs: data[index]) else { return }
+        data.swapAt(parentIndex, index)
+        swim(index: parentIndex)
+    }
+    
+    mutating private func sink(index: Int) {
+        var replaceIndex = index
+        for offset in 1...2 {
+            let childIndex = index * 2 + offset
+            guard childIndex < data.count else { break }
+            guard !order(lhs: data[replaceIndex], rhs: data[childIndex]) else { continue }
+            replaceIndex = childIndex
+        }
+        guard replaceIndex > index else { return }
+        data.swapAt(replaceIndex, index)
+        sink(index: replaceIndex)
+    }
+}
 
 
 // ====================
